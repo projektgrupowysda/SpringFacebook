6299ceff6a839edea1c3eca6ce3a99434abe88ce
Mateusz <mateusz.olewinski96@gmail.com>
2018-10-06 14:36:12 +0200

    creating user

---
+++ src/main/java/com/sda/SpringFacebook/services/CreatingUserService.java
@@ -0,0 +1,29 @@
+package com.sda.SpringFacebook.services;
+
+import com.sda.SpringFacebook.database.UserRepository;
+import com.sda.SpringFacebook.model.User;
+import com.sda.SpringFacebook.request.CreateUserRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreatingUserService {
+
+    private UserRepository repository;
+
+    @Autowired
+    public CreatingUserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createPerson(CreateUserRequest request) {
+        User person = User.builder()
+                .login(request.getLogin())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .password(request.getPassword())
+                .build();
+
+        repository.save(person);
+    }
+}
