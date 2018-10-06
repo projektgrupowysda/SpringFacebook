f835e87f38b00d06cab9cd73b6164427e18b332b
Jacob <dobrogowskijakub@gmail.com>
2018-10-06 14:12:14 +0200

    testowanie

--- src/main/java/com/sda/SpringFacebook/SpringFacebookApplication.java
+++ src/main/java/com/sda/SpringFacebook/SpringFacebookApplication.java
@@ -3,15 +3,14 @@ package com.sda.SpringFacebook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SpringFacebookApplication {
 
 	//porażka 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFacebookApplication.class, args);
+//duza porażka
 
-
-		// Michał
+		
 	}
 }
