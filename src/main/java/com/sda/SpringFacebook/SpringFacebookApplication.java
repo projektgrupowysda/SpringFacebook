3d7598f8249af70781b71a8a33ce61a5df2756e4
Jakub  Dobrogowski <dobrogowskijakub@gmail.com>
2018-10-06 12:55:10 +0200

    porażka

--- src/main/java/com/sda/SpringFacebook/SpringFacebookApplication.java
+++ src/main/java/com/sda/SpringFacebook/SpringFacebookApplication.java
@@ -3,9 +3,11 @@ package com.sda.SpringFacebook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringFacebookApplication {
 
+	//porażka 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFacebookApplication.class, args);
 	}
