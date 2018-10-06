53a01182d389ec1ce2b3b6baa58f93d122b67c9c
Krystian <krystianmaciejczak@gmail.com>
2018-10-06 13:12:49 +0200

    userDB

---
+++ src/main/java/com/sda/SpringFacebook/database/UserRepository.java
@@ -0,0 +1,8 @@
+package com.sda.SpringFacebook.database;
+
+import com.sda.SpringFacebook.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/sda/SpringFacebook/model/User.java
@@ -0,0 +1,27 @@
+package com.sda.SpringFacebook.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@Builder
+@Document(collection = "user")
+public class User {
+
+    @Id
+    private String id;
+    private String login;
+    private String firstName;
+    private String lastName;
+    private String passwordHash;
+
+    private List<User> friends;
+
+
+}
