eff4c1f8b9a68ae6f99eb80b233dac46cd53d93a
Krystian <krystianmaciejczak@gmail.com>
2018-10-06 12:45:57 +0200

    init

---
+++ src/main/java/com/sda/SpringFacebook/SpringFacebookApplication.java
@@ -0,0 +1,12 @@
+package com.sda.SpringFacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacebookApplication.class, args);
+	}
+}
